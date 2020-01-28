e1a10245e0708952ed89b88ec852cfbf7adaa535
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-01-28 22:44:41 +0900

    seeding part for Product, User and Review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
+use App\Model\Product;
 use App\Model\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
         //
+        'user_id' => User::all()->random()->id,
+        'product_id' => Product::all()->random()->id,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5),
     ];
 });
--- database/migrations/2020_01_28_131746_create_reviews_table.php
+++ database/migrations/2020_01_28_131746_create_reviews_table.php
@@ -18,7 +18,7 @@ class CreateReviewsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('product_id');
             $table->text('review');
-            $table->integer('start');
+            $table->integer('star')->default(0);
             $table->timestamps();
 
             $table->foreign('user_id')
