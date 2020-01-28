db72d9f56335839081b7a16c6065c12920960227
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-01-29 00:27:40 +0900

    installed passport

--- app/User.php
+++ app/User.php
@@ -2,14 +2,15 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
