bbb80442efc142171a17d2e5291f93746d5e70ab
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-01-28 23:17:17 +0900

    modifed Product detail

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
+
     /**
      * Transform the resource into an array.
      *
@@ -17,9 +18,15 @@ class ProductResource extends JsonResource
         return [
             'name' => $this->name,
             'description' => $this->detail,
+            'stock' => $this->stock === 0 ? 'Out of stock' : $this->stock,
             'price' => $this->price,
-            'stock' => $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ?
+                round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
